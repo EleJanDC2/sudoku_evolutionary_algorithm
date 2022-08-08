068d7f10c53b27783a034a56aca258ea0d2b96ff
Jan Czaja <elejandc.2@gmail.com>
2022-08-08 16:21:13 +0200

    add canbeset()

--- csudoku.cpp
+++ csudoku.cpp
@@ -4,3 +4,20 @@ cSudoku::cSudoku()
 {
 
 }
+
+bool cSudoku::canbeset(int row, int column,int number)
+{
+    bool canbesetbool = true;
+    //
+    for(int i = 0; i < 9; i++)
+    {
+        if(grid[row][i] == number) canbesetbool = false;
+    }
+    //
+    for(int i = 0; i < 9; i++)
+    {
+        if(grid[i][column] == number) canbesetbool = false;
+    }
+    //
+    return canbesetbool;
+}
--- csudoku.h
+++ csudoku.h
@@ -7,6 +7,7 @@ class cSudoku
 public:
     int grid[9][9];
     cSudoku();
+    bool canbeset(int row, int column, int number);
 };
 
 #endif // CSUDOKU_H
