9c5659c28822fb6740f5e0a372d2070e84dbe2e9
Jan Czaja <elejandc.2@gmail.com>
2022-08-08 16:28:33 +0200

    add test if number is between 1 and 9

--- csudoku.cpp
+++ csudoku.cpp
@@ -9,6 +9,8 @@ bool cSudoku::canbeset(const int row,const int column,const int number)
 {
     bool canbesetbool = true;
     //
+    if(number < 1 || number > 9) canbesetbool = false;
+    //
     for(int i = 0; i < 9; i++)
     {
         if(grid[row][i] == number) canbesetbool = false;
